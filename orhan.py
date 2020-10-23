0a0cedb136b00797e3834664860b110036ac541c
Orhan Nurkan <orhannurkan@hotmail.com>
2020-10-23 16:44:10 +0200

    just have clear data set but already have graphs

--- orhan.py
+++ orhan.py
@@ -1,21 +1,48 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import matplotlib
+import seaborn as sns
 
 # url = 'https://raw.githubusercontent.com/FrancescoMariottini/project3/main/inputs/sales_data.csv'
-url = './inputs/all_sales_data.csv'
+url = './clean_sales_dataset.csv'
 df = pd.read_csv(url)
 print(df)
-print(df.dtypes)
-# # 1- Done... ----- property_subtype  -----
-# -----if the column exist this text delete that line because that records are abroud
-del_subtype = ['Wohnung', 'Triplexwohnung', 'Sonstige', 'Loft / �tico', 'Loft / Dachgeschoss', 'Loft / Attic',
-               'Gewerbe', 'Etagenwohnung', 'Erdgeschoss', 'Attico', 'Appartamento duplex', 'Apartamento', 'Altbauwohnung']
-new_df = df[df['property_subtype'].apply(
-    lambda x: type(x) not in [int, float])]
-new_df = new_df[new_df['property_subtype'].apply(lambda x: "sqft" not in x)]
-new_df = new_df[new_df['property_subtype'].apply(
-    lambda x: x not in del_subtype)]
-# print(new_df['property_subtype'].unique())
+# print(df.dtypes)
+# print(df['area'].unique())
+# # # 1- Done... ----- property_subtype  -----
+# # -----if the column exist this text delete that line because that records are abroud
+# del_subtype = ['Wohnung', 'Triplexwohnung', 'Sonstige', 'Loft / �tico', 'Loft / Dachgeschoss', 'Loft / Attic',
+#                'Gewerbe', 'Etagenwohnung', 'Erdgeschoss', 'Attico', 'Appartamento duplex', 'Apartamento', 'Altbauwohnung']
+# new_df = df[df['property_subtype'].apply(
+#     lambda x: type(x) not in [int, float])]
+# new_df = new_df[new_df['property_subtype'].apply(lambda x: "sqft" not in x)]
+# new_df = new_df[new_df['property_subtype'].apply(
+#     lambda x: x not in del_subtype)]
+# # print(new_df['property_subtype'].unique())
+# # to_renovate = ['TO_RENOVATE',  'TO_BE_DONE_UP', 'TO_RESTORE', 'old',
+# #                'To renovate', 'To be done up', 'To restore']
+# # good = ['GOOD',  'Good', 'AS_NEW', 'As new', ]
+# # just_renovated = ['JUST_RENOVATED', 'Just renovated']
+# # new = ['New']
+# # nan = ['0']
+# # df['new_building_state'] = df['building_state'].apply(
+# #     lambda x: "To renovate" if x in to_renovate else
+# #     ("Good" if x in good else
+# #      ("Just renovated" if x in just_renovated else
+# #       ("New" if x in new else None))))
+
+
+# # # 2- Done... ----- sale -----   # , na=False solved confusing about float
+# new_df = new_df[~new_df['sale'].str.contains('annuity', na=False)]
+
+# # # 3- Done... ----- kitchen_has  -----
+# new_df['garden'] = new_df['garden'].map(
+#     lambda x: x if x == 'True' or x == 'False' else np.nan)
+# #
+
+# # # 4- Done... ----- building_state -----
+# # DO NOT DELETE THIS LINE owner is SARA df.loc[("To be done up" == df['building_state']),"building_state"] = "To_renovate"
 # to_renovate = ['TO_RENOVATE',  'TO_BE_DONE_UP', 'TO_RESTORE', 'old',
 #                'To renovate', 'To be done up', 'To restore']
 # good = ['GOOD',  'Good', 'AS_NEW', 'As new', ]
@@ -27,38 +54,42 @@ new_df = new_df[new_df['property_subtype'].apply(
 #     ("Good" if x in good else
 #      ("Just renovated" if x in just_renovated else
 #       ("New" if x in new else None))))
-
-
-# # 2- Done... ----- sale -----   # , na=False solved confusing about float
-new_df = new_df[~new_df['sale'].str.contains('annuity', na=False)]
-
-# # 3- Done... ----- kitchen_has  -----
-new_df['garden'] = new_df['garden'].map(
-    lambda x: x if x == 'True' or x == 'False' else np.nan)
-#
-
-# # 4- Done... ----- building_state -----
-# DO NOT DELETE THIS LINE owner is SARA df.loc[("To be done up" == df['building_state']),"building_state"] = "To_renovate"
-to_renovate = ['TO_RENOVATE',  'TO_BE_DONE_UP', 'TO_RESTORE', 'old',
-               'To renovate', 'To be done up', 'To restore']
-good = ['GOOD',  'Good', 'AS_NEW', 'As new', ]
-just_renovated = ['JUST_RENOVATED', 'Just renovated']
-new = ['New']
-nan = ['0']
-df['new_building_state'] = df['building_state'].apply(
-    lambda x: "To renovate" if x in to_renovate else
-    ("Good" if x in good else
-     ("Just renovated" if x in just_renovated else
-      ("New" if x in new else None))))
----------------------------------------------------------------------------------------
-# # 5- Done... ----- region -----
-# DO NOT DELETE THIS LINE owner is SARA df.loc[("To be done up" == df['building_state']),"building_state"] = "To_renovate"
-waloon = np.arange(1300, 1500).tolist() + np.arange(4000, 8000).tolist()
-flanders = np.arange(1500, 4000).tolist() + np.arange(8000, 10000).tolist()
-brussels = np.arange(1000, 1300).tolist()
-df['region'] = df['postcode'].apply(
-    lambda x: "W" if x in waloon else("F" if x in flanders else "B"))
-# ---------------------------------------------------------------------------------------
+# # ---------------------------------------------------------------------------------------
+# # # 5- Done... ----- region -----
+# # DO NOT DELETE THIS LINE owner is SARA df.loc[("To be done up" == df['building_state']),"building_state"] = "To_renovate"
+# waloon = np.arange(1300, 1500).tolist() + np.arange(4000, 8000).tolist()
+# flanders = np.arange(1500, 4000).tolist() + np.arange(8000, 10000).tolist()
+# brussels = np.arange(1000, 1300).tolist()
+# df['region'] = df['postcode'].apply(
+#     lambda x: "W" if x in waloon else("F" if x in flanders else "B"))
+# # ---------------------------------------------------------------------------------------
+# # # 6- Done... ----- price_per_area -----
+df['price_per_area'] = df["price"]/df["area"]
+df['price_per_room'] = df["price"]/df["rooms_number"]
+#  IF(x>0 && x<35,"0-35",IF(x>36 && x<60,"36-60",IF(x>61 && x<75,"61-75",IF(x>76 && x<90,"76-90",IF(x>91 && x<120,"91-120",IF(x>121 && x<180,"121-180",IF(x>181 && x<250,"181-250","250+")))))))
+df['area_group'] = df['area'].apply(
+    lambda x: "0-60" if x > 0 and x < 61 else
+    ("61-90" if x > 60 and x < 91 else
+     ("91-120" if x > 90 and x < 121 else
+      ("121-180" if x > 120 and x < 181 else
+       ("181-250" if x > 180 and x < 251 else
+        "250+")))))
+# print(df)
+# .value_counts())
+# print(df.groupby('area_group')[['price_per_area']].median())
+df_new = df.groupby('area_group')[['price_per_area']].median()
+print(df_new)
+df_new = df_new.add_suffix('').reset_index()
+print(df_new)
+df_new = df_new.add_suffix('').reset_index()
+print(df_new)
+# plt.plot(df.groupby('area_group')['area_group'].unique())
+base_x = [0, 1, 2, 3, 4, 5]
+print(base_x)
+sns.lmplot(x='index', y='price_per_area', data=df_new)
+# sns.jointplot(x='Attack', y='Defense', data=df)
+plt.show()
+# print(df['price_per_area'])
 
 
 # anvers = loc.str.contains('Anvers')
